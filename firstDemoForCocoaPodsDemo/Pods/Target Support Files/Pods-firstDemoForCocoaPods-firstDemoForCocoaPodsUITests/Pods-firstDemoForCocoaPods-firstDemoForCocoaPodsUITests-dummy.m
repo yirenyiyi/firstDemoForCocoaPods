ce54e1706bf690a0ecc7bc7a3bd8064c893411e6
wangxin <wangxin@gaodun.com>
2022-01-10 16:23:44 +0800

    修改podfile

---
+++ firstDemoForCocoaPodsDemo/Pods/Target Support Files/Pods-firstDemoForCocoaPods-firstDemoForCocoaPodsUITests/Pods-firstDemoForCocoaPods-firstDemoForCocoaPodsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_firstDemoForCocoaPods_firstDemoForCocoaPodsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_firstDemoForCocoaPods_firstDemoForCocoaPodsUITests
+@end
---
+++ firstDemoForCocoaPodsDemo/Pods/Target Support Files/firstDemoForCocoaPods/firstDemoForCocoaPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_firstDemoForCocoaPods : NSObject
+@end
+@implementation PodsDummy_firstDemoForCocoaPods
+@end
